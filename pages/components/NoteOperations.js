3923d5afbf617b29e5bdd2eecce39de5c2af0a44
Leandro Carrizo <leandropuntomp3@gmail.com>
2023-05-30 12:08:11 -0300

    add description note with react-quill

--- pages/components/NoteOperations.js
+++ pages/components/NoteOperations.js
@@ -2,10 +2,14 @@ import styles from "../../styles/Evernote.module.scss";
 import { useState } from "react";
 import { app, database } from "../../firebaseConfig";
 import { collection, addDoc } from "firebase/firestore";
+const ReactQuill =
+  typeof window === "object" ? require("react-quill") : () => false;
+import "react-quill/dist/quill.snow.css";
 
 export default function NoteOperations() {
   const [inputVisible, setInputVisible] = useState(false);
   const [noteTitle, setNoteTitle] = useState("");
+  const [noteDesc, setNoteDesc] = useState("");
   const dbInstance = collection(database, "notes");
 
   const inputToggle = () => {
@@ -15,7 +19,15 @@ export default function NoteOperations() {
   const saveNote = () => {
     addDoc(dbInstance, {
       noteTitle: noteTitle,
+      noteDesc: noteDesc,
+    }).then(() => {
+      setNoteDesc("");
+      setNoteTitle("");
     });
+  };
+
+  const addDesc = (value) => {
+    setNoteDesc(value);
   };
 
   return (
@@ -33,6 +45,9 @@ export default function NoteOperations() {
             onChange={(event) => setNoteTitle(event.target.value)}
             placeholder="Enter the title..."
           />
+          <div className={styles.ReactQuill}>
+            <ReactQuill onChange={addDesc} />
+          </div>
           <button className={styles.saveBtn} onClick={saveNote}>
             Save Note
           </button>
