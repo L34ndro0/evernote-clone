af06cc63e47fd2b237f87c394afe9f07a52d7270
Leandro Carrizo <leandropuntomp3@gmail.com>
2023-05-30 22:35:13 -0300

    buttons edit and delete note, show edit information

--- pages/components/NoteDetails.js
+++ pages/components/NoteDetails.js
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
 import { app, database } from "../../firebaseConfig";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, getDocs,collection, updateDoc } from "firebase/firestore";
 const ReactQuill =
   typeof window === "object" ? require("react-quill") : () => false;
 import "react-quill/dist/quill.snow.css";
+import styles from "../../styles/Evernote.module.scss";
 
 export default function NoteDetails({ ID }) {
   const [singleNote, setSingleNote] = useState({});
+  const [noteTitle, setNoteTitle] = useState("");
+  const [noteDesc, setNoteDesc] = useState("");
+  const [isEdit, setIsEdit] = useState(false);
 
   useEffect(() => {
     getSingleNote();
@@ -20,10 +24,38 @@ export default function NoteDetails({ ID }) {
     }
   };
 
+  const getEditData = () => {
+    setIsEdit(true);
+    setNoteTitle(singleNote.noteTitle);
+    setNoteDesc(singleNote.noteDesc);
+  };
+
   return (
     <>
+      <div>
+        <button className={styles.editBtn} onClick={() => getEditData()}>
+          Edit
+        </button>
+        <button className={styles.deleteBtn}>Delete</button>
+      </div>
       <h2>{singleNote.noteTitle}</h2>
       <div dangerouslySetInnerHTML={{ __html: singleNote.noteDesc }}></div>
+      {isEdit ? (
+        <div className={styles.inputContainer}>
+          <input
+            className={styles.input}
+            placeholder="Enter the title..."
+            onChange={(event) => setNoteTitle(event.target.value)}
+            value={noteTitle}
+          />
+          <div className={styles.ReactQuill}>
+            <ReactQuill value={noteDesc} onChange={setNoteDesc} />
+          </div>
+          <button className={styles.saveBtn}>Update note</button>
+        </div>
+      ) : (
+        <></>
+      )}
     </>
   );
 }
