bd562073db44d9d946cfad47db8f5ef504a7f23a
Leandro Carrizo <leandropuntomp3@gmail.com>
2023-05-31 17:21:36 -0300

    .env

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDpW5-6VFwsmARhbIjFogvExOL8SL7AgsE",
-  authDomain: "evernoteclone-d20b4.firebaseapp.com",
-  projectId: "evernoteclone-d20b4",
-  storageBucket: "evernoteclone-d20b4.appspot.com",
-  messagingSenderId: "20035973525",
-  appId: "1:20035973525:web:61b5b24bfbd86eaa6e469c",
-  measurementId: "G-PYYHXJ129G",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
